fd052cf0eca3f662b60d1d82fde61640f989d92d
Peter <peter.prentiss@gmail.com>
2017-09-12 17:37:08 -0500

    Create school class library.

---
+++ SchoolApp/SchoolLibrary/School.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SchoolLibrary
+{
+    public class School
+    {
+        string SchoolName;
+        string SchoolAddress;
+        string SchoolCity;
+        string SchoolState;
+        string SchoolZip;
+        string SchoolPhoneNumber;
+    }
+}
