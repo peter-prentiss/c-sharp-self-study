ccfb17aa4e8fffb8bd915a6bc3c1b46b007e7df9
Peter <peter.prentiss@gmail.com>
2017-09-16 17:43:00 -0500

    Change fields to auto properties.

--- SchoolApp/SchoolLibrary/School.cs
+++ SchoolApp/SchoolLibrary/School.cs
@@ -3,11 +3,11 @@ namespace SchoolLibrary
 {
     public class School
     {
-        string SchoolName;
-        string SchoolAddress;
-        string SchoolCity;
-        string SchoolState;
-        string SchoolZip;
-        string SchoolPhoneNumber;
+        string Name { get; set;  }
+        string Address { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+        string Zip { get; set; }
+        string PhoneNumber { get; set; }
     }
 }
