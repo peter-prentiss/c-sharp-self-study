d906cac63fbfa8b2a15fac60f7aa524e812ead81
Peter <peter.prentiss@gmail.com>
2017-09-17 17:48:29 -0500

    Create getters and setters for twitter address property

--- SchoolApp/SchoolLibrary/School.cs
+++ SchoolApp/SchoolLibrary/School.cs
@@ -9,5 +9,20 @@ namespace SchoolLibrary
         string State { get; set; }
         string Zip { get; set; }
         string PhoneNumber { get; set; }
+
+        string _twitterAddress;
+        string TwitterAddress
+        {
+            get { return _twitterAddress; }
+            set 
+            {
+                if (value.StartsWith("@"))
+                {
+                    _twitterAddress = value;
+                } else {
+                    throw new Exception("The Twitter address must begin with '@");
+                }
+            }
+        }
     }
 }
