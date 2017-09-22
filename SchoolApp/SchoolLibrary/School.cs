e7d6ef31e7c171de764f5b9f28a47e203100e842
Peter <peter.prentiss@gmail.com>
2017-09-22 17:59:43 -0500

    Convert to funcion bodied expression.

--- SchoolApp/SchoolLibrary/School.cs
+++ SchoolApp/SchoolLibrary/School.cs
@@ -37,11 +37,13 @@ namespace SchoolLibrary
             PhoneNumber = SchoolPhoneNumber;
         }
 
-        public float AverageThreeScores (float a, float b, float c)
-        {
-            var result = (a + b + c) / 3;
-            return result;
-        }
+        //public float AverageThreeScores (float a, float b, float c)
+        //{
+        //    var result = (a + b + c) / 3;
+        //    return result;
+        //}
+
+        public float AverageThreeScores(float a, float b, float c) => (a + b + c) / 3;
 
         public int AverageThreeScores (int a, int b, int c)
         {
