ad9c1fb7f512fbd176e86c2fee2dc314b5ca56ae
Peter <peter.prentiss@gmail.com>
2017-09-24 18:07:39 -0500

    Override with custom ToString;

--- SchoolApp/SchoolLibrary/School.cs
+++ SchoolApp/SchoolLibrary/School.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+
 namespace SchoolLibrary
 {
     public class School
@@ -49,6 +51,20 @@ namespace SchoolLibrary
         {
             var result = (a + b + c) / 3;
             return result;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(Name);
+            sb.AppendLine(Address);
+            sb.AppendLine(City);
+            sb.AppendLine(",");
+            sb.AppendLine(State);
+            sb.AppendLine(" ");
+            sb.AppendLine(Zip);
+
+            return sb.ToString();
         }
     }
 }
