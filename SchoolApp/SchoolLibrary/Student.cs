6e6baeef411909544dca361a5e43a51c52607e53
Peter <peter.prentiss@gmail.com>
2017-10-01 19:02:26 -0500

    Create Teacher and student classes.

---
+++ SchoolApp/SchoolLibrary/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SchoolLibrary
+{
+    public class Student
+    {
+        public Student()
+        {
+        }
+    }
+}
---
+++ SchoolApp/SchoolLibrary/Teacher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SchoolLibrary
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
