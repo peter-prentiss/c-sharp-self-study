9e65f3e9d62a9c7e7e65f822642758f5cd5a16dc
Peter <peter.prentiss@gmail.com>
2017-10-05 19:19:28 -0500

    Create virtual method.

--- SchoolApp/SchoolLibrary/Person.cs
+++ SchoolApp/SchoolLibrary/Person.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+
 namespace SchoolLibrary
 {
     public abstract class Person
@@ -8,5 +10,16 @@ namespace SchoolLibrary
         public string Email { get; set; }
 
         public abstract float ComputeGradeAverage();
+
+        public virtual string SendMessage(string message)
+        {
+            var sb = new StringBuilder();
+            var timeStamp = string.Format("Sent on {0:D} at {0:t}", DateTime.Now);
+            sb.AppendLine(timeStamp);
+            sb.AppendLine("");
+            sb.AppendLine("Dear " + FirstName + ",");
+            sb.AppendLine(message);
+            return sb.ToString();
+        }
     }
 }
