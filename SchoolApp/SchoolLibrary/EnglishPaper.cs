42c8c3922dd1a72a66eec6570007a318daeaaab3
Peter <peter.prentiss@gmail.com>
2017-10-07 19:24:44 -0500

    Create englishpaper and scienceexperiment classes.

---
+++ SchoolApp/SchoolLibrary/EnglishPaper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SchoolLibrary
+{
+    public class EnglishPaper
+    {
+        public string Title { get; set; }
+        public int MinimumWordCount { get; set; }
+        public string PaperText { get; set; }
+    }
+}
---
+++ SchoolApp/SchoolLibrary/ScienceExperiment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SchoolLibrary
+{
+    public class ScienceExperiment
+    {
+        public string Hypothesis { get; set; }
+        public string Materials { get; set; }
+        public string Method { get; set; }
+        public string Conclusion { get; set; }
+    }
+}
