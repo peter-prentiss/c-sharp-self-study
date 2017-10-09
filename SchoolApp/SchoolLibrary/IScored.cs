3c754eb490c832a73c0d90f9aef71b7c44453c87
Peter <peter.prentiss@gmail.com>
2017-10-08 19:28:12 -0500

    Create score interface and implement in scienceexperiment class.

---
+++ SchoolApp/SchoolLibrary/IScored.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace SchoolLibrary
+{
+    public interface IScored
+    {
+        float Score { get; set; }
+        float MaximumScore { get; set;  }
+    }
+}
--- SchoolApp/SchoolLibrary/ScienceExperiment.cs
+++ SchoolApp/SchoolLibrary/ScienceExperiment.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 namespace SchoolLibrary
 {
-    public class ScienceExperiment
+    public class ScienceExperiment : IScored
     {
         public string Hypothesis { get; set; }
         public string Materials { get; set; }
         public string Method { get; set; }
         public string Conclusion { get; set; }
+        public float Score { get; set; }
+        public float MaximumScore { get; set; }
     }
 }
