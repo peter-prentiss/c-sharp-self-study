ba68a7f05681224d4f97ce10571371a5120ad8e9
Peter <peter.prentiss@gmail.com>
2017-10-23 21:15:43 -0500

    Trying ternary and other if else types.

--- EssentialTraining/EssentialTraining/FlowControl.cs
+++ EssentialTraining/EssentialTraining/FlowControl.cs
@@ -3,6 +3,22 @@ namespace EssentialTraining
 {
     public class FlowControl
     {
+        public bool IsYourFavoriteColorYellow(string color)
+        {
+            return color.ToLower() == "yellow";
+        }
+
+        public bool IsYourFavoriteColorGreen(string color)
+        {
+            return (color.ToLower() == "green") ? true : false;
+        }
+
+        public bool IsYourFavoriteColorRed(string color)
+        {
+            if (color.ToLower() == "red") return true;
+            return false;
+        }
+
         public bool IsYourFavoriteColorBlue(string color) {
             if (color.ToLower() == "blue")
             {
