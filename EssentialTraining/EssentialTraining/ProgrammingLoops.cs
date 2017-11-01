4dccd81da6b24e1c222a1a0683aea56c6ef97aee
Peter <peter.prentiss@gmail.com>
2017-10-31 21:33:59 -0500

    Trying a basic for loop/

---
+++ EssentialTraining/EssentialTraining/ProgrammingLoops.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace EssentialTraining
+{
+    public class ProgrammingLoops
+    {
+        public void ForLoop() 
+        {
+            var sum = 0;
+            for (var i = 0; i < 100; i++)
+            {
+                sum = sum + 1;
+            }
+        }
+    }
+}
