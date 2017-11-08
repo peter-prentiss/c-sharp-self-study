e7fef290565d8df3cf1534459dbf358023970646
Peter <peter.prentiss@gmail.com>
2017-11-07 19:12:55 -0600

    Create Hello World program.

---
+++ HelloConsole/HelloConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Console!");
+
+            Console.ReadKey();
+        }
+    }
+}
