e6c6c780df8ac75b27dc655ca40dd1dd73a9ca0a
Peter <peter.prentiss@gmail.com>
2018-02-23 21:36:39 -0600

    For loop now returns int.

--- EssentialTraining/EssentialTraining/ProgrammingLoops.cs
+++ EssentialTraining/EssentialTraining/ProgrammingLoops.cs
@@ -3,13 +3,15 @@ namespace EssentialTraining
 {
     public class ProgrammingLoops
     {
-        public void ForLoop() 
+        public int ForLoop() 
         {
             var sum = 0;
             for (var i = 0; i < 100; i++)
             {
                 sum = sum + 1;
             }
+
+            return sum;
         }
     }
 }
