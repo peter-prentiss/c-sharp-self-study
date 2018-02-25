88d215f95b65163b396f6f8c5f710d776b11413b
Peter <peter.prentiss@gmail.com>
2018-02-24 21:51:21 -0600

    Trying out do and dowhile loops

--- EssentialTraining/EssentialTraining/ProgrammingLoops.cs
+++ EssentialTraining/EssentialTraining/ProgrammingLoops.cs
@@ -5,6 +5,32 @@ namespace EssentialTraining
 {
     public class ProgrammingLoops
     {
+        public int WhileLoop()
+        {
+            var sum = 0;
+            var counter = 0;
+            while (counter < 100)
+            {
+                sum += counter;
+                counter++;
+            }
+
+            return sum;
+        }
+
+        public int DoWhileLoop()
+        {
+            var sum = 0;
+            var counter = 0;
+            do
+            {
+                sum += counter;
+                counter++;
+            } while (counter < 100);
+
+            return sum;
+        }
+
         public int ForEachLoop()
         {
             var numbers = new List<int> {1, 3, 5, 7, 9};
