f037c79d5793797efa8cf1a13fc968a8349f6c92
Peter <peter.prentiss@gmail.com>
2018-02-24 21:48:51 -0600

    Trying foreach loop.

--- EssentialTraining/EssentialTraining/ProgrammingLoops.cs
+++ EssentialTraining/EssentialTraining/ProgrammingLoops.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace EssentialTraining
 {
     public class ProgrammingLoops
     {
+        public int ForEachLoop()
+        {
+            var numbers = new List<int> {1, 3, 5, 7, 9};
+            var sum = 0;
+            foreach(var number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+
         public int ForLoop() 
         {
             var sum = 0;
