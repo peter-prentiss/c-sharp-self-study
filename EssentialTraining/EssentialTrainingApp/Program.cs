69c8560d9d7a4e3f02f04fba503c3a37e42bbfcd
Peter <peter.prentiss@gmail.com>
2018-02-24 22:24:26 -0600

    Initialize list.

--- EssentialTraining/EssentialTrainingApp/Program.cs
+++ EssentialTraining/EssentialTrainingApp/Program.cs
@@ -10,6 +10,7 @@ namespace EssentialTrainingApp
 
         public static void Main(string[] args)
         {
+            Words = new List<string>();
             Words.Add("Bread");
             Words.Add("Milk");
             Words.Add("Cheese");
